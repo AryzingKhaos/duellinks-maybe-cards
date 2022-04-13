ab96d6481da7cc5589b412c00258ce03d93dc900
Aaron <eesorby@163.com>
2022-04-13 12:44:25 +0800

    测试提交

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /*
  * @Date: 2022-04-13 11:11:54
  * @LastEditors: xiongzeluo
- * @LastEditTime: 2022-04-13 11:24:13
- * @FilePath: /generationNovel/Users/luoxiongze/files/我的坚果云/代码库/duellinks-maybe-cards/src/index.js
+ * @LastEditTime: 2022-04-13 12:43:06
+ * @FilePath: /generationNovel/Users/luoxiongze/code/duellinks-maybe-cards/src/index.js
  */
 
 
 (() => {
-  console.log('aaaa');
+  console.log('aaaa测试提交');
 })();
 
