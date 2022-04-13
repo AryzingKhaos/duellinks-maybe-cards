4805051369dfdf7f1596c97408ae332c727c8475
Aaron <aaron4lawliet@gmail.com>
2022-04-13 12:25:51 +0800

    初始化项目，添加配置信息

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Date: 2022-04-13 11:11:54
+ * @LastEditors: xiongzeluo
+ * @LastEditTime: 2022-04-13 11:24:13
+ * @FilePath: /generationNovel/Users/luoxiongze/files/我的坚果云/代码库/duellinks-maybe-cards/src/index.js
+ */
+
+
+(() => {
+  console.log('aaaa');
+})();
+
